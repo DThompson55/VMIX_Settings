c6fba1e14e1251442371c1413ee2d94d19447fdd
Daniel Thompson <danl_thompson@hotmail.com>
2022-08-18 15:51:23 -0400

    First Release

---
+++ README.md
@@ -0,0 +1,2 @@
+# VMIX-Settings
+A utility to pull input numbers and short names into a CSV file.
---
+++ src/AxiosWrapper.js
@@ -0,0 +1,13 @@
+const axios = require('axios').default;
+
+var host = "http://localhost:8088"; // could parameterize this TTD
+//var host = "http://100.76.100.40:8088"; // could parameterize this TTD
+
+async function vMixSend(endpoint,params) {
+      const response = axios.get(host+endpoint, {params})
+      return response;
+} 
+
+exports.vMixSend = vMixSend
+
+//UnhandledPromiseRejectionWarning: Error: connect ECONNREFUSED 127.0.0.1:8088
